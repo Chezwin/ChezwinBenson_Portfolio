f6f1cbd83c65c47da6357004443a109e2ac4af08
Chezwin Benson <chezwinbenson@gmail.com>
2025-11-29 09:58:42 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -115,3 +115,15 @@ window.addEventListener("scroll", function() {
 });
 </script>
 
+<script>
+document.addEventListener("scroll", function () {
+    const nav = document.querySelector("body");
+    if (window.scrollY > 10) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+</script>
+
+
